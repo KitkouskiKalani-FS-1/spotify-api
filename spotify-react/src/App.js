7f4b259da93886185feaf418b712db17137d6352
KitkouskiKalani-FS-1 <KMKitkouski@student.fullsail.edu>
2022-08-08 19:37:51 -0500

    Added React frontend and linked the backend and frontend

---
+++ spotify-react/src/App.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from "react";
+import './App.css';
+
+function App() {
+  const [info, setInfo] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+
+  const API_BASE = process.env.NODE_ENV ==='development'
+  ? `http://localhost:3001`
+  : process.env.REACT_APP_BASE_URL;
+
+  useEffect(()=>{
+    let ignore = false
+
+    if(!ignore){
+      getHome();
+    }
+
+    return()=>{
+      ignore = true;
+    }
+  }, [])
+
+  const getHome = async() =>{
+    try{
+      await fetch(`${API_BASE}/spotify`)
+        .then(res => res.json())
+        .then(data=>{
+          console.log(data)
+          setInfo(data)
+        })
+    } catch{
+      setError(error|| "Unexpected Error")
+    } finally{
+      setLoading(false)
+    }
+  
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        {
+          <h1>{info.header}</h1>
+        }
+      </header>
+    </div>
+  );
+}
+
+export default App;
